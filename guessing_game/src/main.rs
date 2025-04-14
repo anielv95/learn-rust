7c17d712d7ba116d877a8dc4ef7a32f29532466d
aniel <anielvillegas@gmail.com>
2025-04-14 12:15:42 +0000

    update main.rs in guessing program

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -6,20 +6,20 @@ fn main() {
     println!("Guess the number!");
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {secret_number}");
-    println!("Please input your guess.");
-    let mut guess = String::new();
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    let guess: u32 = match guess.trim().parse() {
-        Ok(num) => num,
-        Err(_) => continue,
-    };
-
-    println!("You guessed: {guess}" );
-
+    
     loop {
         println!("Please input your guess.");
+        let mut guess = String::new();
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guessed: {guess}" );
+
 
         // --snip--
         
